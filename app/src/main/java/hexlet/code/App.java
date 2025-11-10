5692f432bb941fa333ca1136d303905d7c60373c
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-10 17:10:15 +0300

    feature : add an information about utility
    * Added an information about utility

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Callable<Integer> {
+
+    @Parameters(paramLabel = "filePath1")
+    private String filePath1;
+
+    @Parameters(paramLabel = "filePath2")
+    private String filePath2;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
