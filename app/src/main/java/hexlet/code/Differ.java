3efe0ea91d6b756d2aee2291016ae3fb06b2a201
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-10 19:05:50 +0300

    feature : add a differ class
    * Added a differ class

---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+public class Differ {
+
+    /**
+     * Метод сравнивает два файла и генерирует на их основе строку в выбранном формате
+     * @param filepath1 путь до первого файла
+     * @param filepath2 путь до второго файла
+     * @param format формат текста, который должен быть на выходе
+     * @return сгенерированная строка
+     * @throws Exception исключение, которое может возникнуть во время работы
+     */
+    public static String generate(String filepath1, String filepath2, String format) throws Exception {
+        // TODO: Отредактировать логику
+        return null;
+    }
+}
