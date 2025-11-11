8e8c1bf9a3c29a146298491d8d0d2fbed7e32dc5
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-11 10:14:03 +0300

    feature : add a logic to get difference
    * Added a logic to get difference

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,17 +1,88 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Differ {
 
     /**
      * Метод сравнивает два файла и генерирует на их основе строку в выбранном формате
+     *
      * @param filepath1 путь до первого файла
      * @param filepath2 путь до второго файла
-     * @param format формат текста, который должен быть на выходе
+     * @param format    формат текста, который должен быть на выходе
      * @return сгенерированная строка
      * @throws Exception исключение, которое может возникнуть во время работы
      */
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
-        // TODO: Отредактировать логику
-        return null;
+        // Читаем содержимое файлов
+        String content1 = Files.readString(Paths.get(filepath1));
+        String content2 = Files.readString(Paths.get(filepath2));
+
+        // Определяем формат файлов по расширению
+        String fileFormat = getFileFormat(filepath1);
+
+        // Парсинг файлов в Map
+        Map<String, Object> data1 = Parser.parsingFormat(content1, fileFormat);
+        Map<String, Object> data2 = Parser.parsingFormat(content2, fileFormat);
+
+        // Собираем все ключи и сортируем
+        Set<String> allKeys = new TreeSet<>();
+        allKeys.addAll(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        StringBuilder result = new StringBuilder("{\n");
+
+        for (String key : allKeys) {
+            boolean inFile1 = data1.containsKey(key);
+            boolean inFile2 = data2.containsKey(key);
+
+            if (inFile1 && inFile2) {
+                Object value1 = data1.get(key);
+                Object value2 = data2.get(key);
+
+                if (isEqual(value1, value2)) {
+                    // Значения одинаковые
+                    result.append("    ").append(key).append(": ").append(value1).append("\n");
+                } else {
+                    // Значения разные - сначала из первого файла, потом из второго
+                    result.append("  - ").append(key).append(": ").append(value1).append("\n");
+                    result.append("  + ").append(key).append(": ").append(value2).append("\n");
+                }
+
+            } else if (inFile1) {
+                // Ключ только в первом файле
+                result.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
+            } else {
+                // Ключ только во втором файле
+                result.append("  - ").append(key).append(": ").append(data2.get(key)).append("\n");
+            }
+        }
+
+        result.append("}");
+        return result.toString();
+    }
+
+    private static String getFileFormat(String filepath) {
+        if (filepath.endsWith(".json")) {
+            return "json";
+        } else if (filepath.endsWith(".yml") || filepath.endsWith(".yaml")) {
+            return "yaml";
+        }
+
+        throw new IllegalArgumentException("Unsupported file format: " + filepath);
+    }
+
+    private static boolean isEqual(Object obj1, Object obj2) {
+        if (obj1 == null && obj2 == null) {
+            return true;
+        }
+        if (obj1 == null || obj2 == null) {
+            return false;
+        }
+        return obj1.equals(obj2);
     }
 }
