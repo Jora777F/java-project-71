3676080ba0a32435ef615a366382ba6b779961fa
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-14 17:41:47 +0300

    fix : fix the checkstyle violations
    * Fixed the checkstyle violations

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -8,10 +8,10 @@ class AppTest {
     @Test
     public void testCommandLineArguments() {
         String[] args = {
-                "src/test/resources/fixtures/file1.json ",
-                "src/test/resources/fixtures/file2.json ",
-                "-f",
-                "stylish"
+            "src/test/resources/fixtures/file1.json ",
+            "src/test/resources/fixtures/file2.json ",
+            "-f",
+            "stylish"
         };
         App app = new App();
         new CommandLine(app).parseArgs(args);
