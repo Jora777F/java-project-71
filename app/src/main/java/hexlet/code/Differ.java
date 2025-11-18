fa38774fb7b6de835d23b940d50bc83580574c83
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-18 11:36:24 +0300

    refactor : edit the differ class
    * Edited the differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -5,12 +5,8 @@ import hexlet.code.formatters.Formatter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Differ {
 
@@ -43,54 +39,15 @@ public class Differ {
         String content1 = Files.readString(path1);
         String content2 = Files.readString(path2);
 
-        // Определяем формат файлов
         String fileFormat = getFileFormat(filepath1);
 
         // Парсинг файлов в Map
         Map<String, Object> data1 = Parser.parsingFormat(content1, fileFormat);
         Map<String, Object> data2 = Parser.parsingFormat(content2, fileFormat);
 
-        List<DiffNode> diff = buildDiff(data1, data2);
+        List<DiffNode> diff = DiffBuilder.buildDiff(data1, data2);
 
         return Formatter.format(diff, format);
-    }
-
-    /**
-     * Вычисляет различия между файлами и возвращает список {@link DiffNode}.
-     *
-     * @param data1 первая структура
-     * @param data2 вторая структура
-     * @return список с различиями
-     */
-    private static List<DiffNode> buildDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        List<DiffNode> diff = new ArrayList<>();
-
-        // Собираем все ключи и сортируем
-        Set<String> allKeys = new TreeSet<>();
-        allKeys.addAll(data1.keySet());
-        allKeys.addAll(data2.keySet());
-
-        for (String key : allKeys) {
-            boolean inFile1 = data1.containsKey(key);
-            boolean inFile2 = data2.containsKey(key);
-
-            if (inFile1 && inFile2) {
-                Object value1 = data1.get(key);
-                Object value2 = data2.get(key);
-
-                if (Objects.equals(value1, value2)) {
-                    diff.add(new DiffNode(key, value1, value2, DiffNode.Status.UNCHANGED));
-                } else {
-                    diff.add(new DiffNode(key, value1, value2, DiffNode.Status.CHANGED));
-                }
-            } else if (inFile1) {
-                diff.add(new DiffNode(key, data1.get(key), null, DiffNode.Status.REMOVED));
-            } else {
-                diff.add(new DiffNode(key, null, data2.get(key), DiffNode.Status.ADDED));
-            }
-        }
-
-        return diff;
     }
 
     /**
